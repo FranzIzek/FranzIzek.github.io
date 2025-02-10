c4c3db96839cdc957327d79f278ee8599f57e0a5
FranzIzek <167728761+FranzIzek@users.noreply.github.com>
2025-02-10 18:20:56 +0800

    Update tsa2.js

--- TSA2/tsa2.js
+++ TSA2/tsa2.js
@@ -1,12 +1,28 @@
 const buttons = document.querySelectorAll('button');
-const boxcolor = document.querySelector('.boxcolor');
-
+let box1 = null;
 
 buttons.forEach(button => {
     button.addEventListener('click', () => {
-        const buttonColor = window.getComputedStyle(button).backgroundColor;
-        boxcolor.style.backgroundColor = buttonColor;
+        let color = window.getComputedStyle(button).backgroundColor;
+
+        if (box1) {
+            let box2 = box1;
+            box2.style.transform = 'translateX(20px) translateY(-800px)';
+
+            setTimeout(() => {
+                box2.remove();
+            }, 1500);
+        }
+
+        let newBox = document.createElement('div');
+        newBox.classList.add('box');
+        newBox.style.backgroundColor = color;
+        document.querySelector('.box-container').appendChild(newBox);
+
+        setTimeout(() => {
+            newBox.style.transform = 'translateX(20px) translateY(50px)';
+        }, 50);
+
+        box1 = newBox;
     });
 });
-
-
